616350c5b85705e456518cb2ebcaee6b64a6597d
%%GITNAME%% <%%GITEMAIL%%>
2016-05-03 14:17:02 -0700

    forgot HEXing

--- lab-justin/lib/lab03.js
+++ lab-justin/lib/lab03.js
@@ -91,21 +91,21 @@ module.exports = function() {
     oneBuf = new Buffer(data).slice(0, 8);
     if (err) throw err;
     console.log(oneBuf.toString('utf8'));
-    // console.log(oneBuf.toString('hex'));
+    console.log(oneBuf.toString('hex'));
   });
   process.nextTick(function(){
     fs.readFile('./data/two.txt', function(err, data) {
       twoBuf = new Buffer(data).slice(0, 8);
       if (err) throw err;
       console.log(twoBuf.toString('utf8'));
-      // console.log(twoBuf.toString('hex'));
+      console.log(twoBuf.toString('hex'));
 
       process.nextTick(function() {
         fs.readFile('./data/three.txt', function(err, data) {
           threeBuf = new Buffer(data).slice(0, 8);
           if (err) throw err;
           console.log(threeBuf.toString('utf8'));
-          // console.log(threeBuf.toString('hex'));
+          console.log(threeBuf.toString('hex'));
 
         });
       });

8c1fa0dd5cbe5d4479a467e8bd20e2cdb1cf7129
%%GITNAME%% <%%GITEMAIL%%>
2016-05-03 12:42:23 -0700

    refactored - check for false positive test

--- lab-justin/lib/lab03.js
+++ lab-justin/lib/lab03.js
@@ -1,9 +1,45 @@
 'use strict';
 
+// module.exports = function() {
+//   // const input = something;
+//   const fs = require('fs');
+//   var oneBuf, twoBuf, threeBuf;
+//
+//   fs.readFile('./data/one.txt', function(err, data) {
+//     oneBuf = new Buffer(data).slice(0, 8);
+//     if (err) throw err;
+//     console.log(oneBuf.toString('utf8'));
+//     console.log(oneBuf.toString('hex'));
+//     console.log(oneBuf.toString('base64'));
+//
+//   });
+//
+//   fs.readFile('./data/two.txt', function(err, data) {
+//     twoBuf = new Buffer(data).slice(0, 8)
+//     if (err) throw err;
+//     console.log(twoBuf.toString('utf8'));
+//     console.log(twoBuf.toString('hex'));
+//     console.log(oneBuf.toString('base64'));
+//
+//   });
+//
+//   fs.readFile('./data/three.txt', function(err, data) {
+//     threeBuf = new Buffer(data).slice(0, 8)
+//     if (err) throw err;
+//     console.log(threeBuf.toString('utf8'));
+//     console.log(threeBuf.toString('hex'));
+//     console.log(oneBuf.toString('base64'));
+//
+//     });
+//   return 'I hate this class!';
+// };
+
+
 module.exports = function() {
   // const input = something;
   const fs = require('fs');
   var oneBuf, twoBuf, threeBuf;
+
   fs.readFile('./data/one.txt', function(err, data) {
     oneBuf = new Buffer(data).slice(0, 8);
     if (err) throw err;
@@ -11,24 +47,37 @@ module.exports = function() {
     console.log(oneBuf.toString('hex'));
     console.log(oneBuf.toString('base64'));
 
-  });
+    // return twoBuf;
 
-  fs.readFile('./data/two.txt', function(err, data) {
-    twoBuf = new Buffer(data).slice(0, 8)
-    if (err) throw err;
-    console.log(twoBuf.toString('utf8'));
-    console.log(twoBuf.toString('hex'));
-    console.log(oneBuf.toString('base64'));
+  // });
 
-  });
+  // function twoBuf() {
+    return fs.readFile('./data/two.txt', function(err, data) {
+      twoBuf = new Buffer(data).slice(0, 8);
+      if (err) throw err;
+      console.log(twoBuf.toString('utf8'));
+      console.log(twoBuf.toString('hex'));
+      console.log(twoBuf.toString('base64'));
 
-  fs.readFile('./data/three.txt', function(err, data) {
-    threeBuf = new Buffer(data).slice(0, 8)
-    if (err) throw err;
-    console.log(threeBuf.toString('utf8'));
-    console.log(threeBuf.toString('hex'));
-    console.log(oneBuf.toString('base64'));
+      // return threeBuf;
 
+    // });
+  // }
+
+  // function threeBuf() {
+      return fs.readFile('./data/three.txt', function(err, data) {
+        threeBuf = new Buffer(data).slice(0, 8);
+        if (err) throw err;
+        console.log(threeBuf.toString('utf8'));
+        console.log(threeBuf.toString('hex'));
+        console.log(threeBuf.toString('base64'));
+
+    // });
+  // };
+// };
+
+      });
     });
+  });
   return 'I hate this class!';
 };

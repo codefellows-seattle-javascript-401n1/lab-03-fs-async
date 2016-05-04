5bc709f5aea2570965bebafc6f39e3e822dc4eca
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-05-04 13:28:34 -0700

    add read function

--- lab-anthony/lib/read.js
+++ lab-anthony/lib/read.js
@@ -1,3 +1,18 @@
 'use strict';
 
 const fs = require('fs');
+
+module.exports = function(pathsArray) {
+  var dataArray = [];
+  pathsArray.forEach(function(file, index){
+    fs.readFile(file, (err, data) => {
+      if (err) throw err;
+      dataArray[index] = data.toString('hex', 0, 8);
+
+      if (pathsArray.length === dataArray.length)
+        dataArray.forEach(function(hexdata){
+          console.log(hexdata);
+        });
+    });
+  });
+};

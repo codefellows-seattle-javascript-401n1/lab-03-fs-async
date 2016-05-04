fa4a57240858eee8437412c0a4c3de8a58650aae
ANTHONY CASSON <ANTHONY.CASSON@GMAIL.COM>
2016-05-04 13:29:17 -0700

    add read function test

---
+++ lab-anthony/test/read-test.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const expect = require('chai').expect;
+const fs = require('fs');
+
+describe('Testing read module', function() {
+  describe('Testing read() file reading', function() {
+    it('Should error', function(done) {
+      var pathsArray = [
+        __dirname + '/../data/nothing.txt'];
+
+      pathsArray.forEach(function(file){
+        fs.readFile(file, (err, data) => {
+          expect(err).to.be.equal(err);
+          expect(data).to.be.equal(undefined);
+          done();
+        });
+      });
+    });
+  });
+});
+
+describe('Testing read module', function() {
+  describe('Testing read()', function() {
+    it('Should list buffers in order', function(done) {
+      var pathsArray = [
+        __dirname + '/../data/first.txt',
+        __dirname + '/../data/second.txt',
+        __dirname + '/../data/third.txt'];
+      var dataArray = [];
+
+      pathsArray.forEach(function(file, index){
+        fs.readFile(file, (err, data) => {
+          if (err) throw err;
+          dataArray[index] = data.toString('hex', 0, 8);
+
+          if (pathsArray.length === dataArray.length) {
+            expect(dataArray[0]).to.be.equal('46697273743a2054');
+            expect(dataArray[1]).to.be.equal('5365636f6e643a20');
+            expect(dataArray[2]).to.be.equal('54686972643a2042');
+            done();
+          }
+        });
+      });
+    });
+  });
+});

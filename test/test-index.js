6b54e8fdead38c5b363f6515dab7d5584301a2e0
ron oh <ronoh2001@gmail.com>
2016-05-04 23:40:24 -0700

    updated and changed all

---
+++ test/test-index.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const data = require(__dirname +'/../index');
+const expect = require('chai').expect;
+// const fs = require('fs');
+
+describe('testing index.js', function(){
+  describe('testing fs.readFile', function(){
+    it('should pass back a result array with three 8 bit hex sequences', function(done){
+      expect(data.toString('hex', 0, 8)).to.be.equal('[ "5261706861656c2d", "4d69636861656c61", "4c656f6e6172646f" ]');
+        // expect(true).to equal(true)
+      done();
+    });
+  });
+});

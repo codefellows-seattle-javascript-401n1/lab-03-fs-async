a4697106f5abde089d9734765af1e7b59a7a5dfd
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-05-04 18:37:30 -0700

    fixed gulpfile, no errors and proper watch usage

--- lab-jacob/gulpfile.js
+++ lab-jacob/gulpfile.js
@@ -5,7 +5,7 @@ const mocha = require('gulp-mocha');
 const eslint = require('gulp-eslint');
 
 gulp.task('test', function() {
-  return gulp.src('test/read-test.js')
+  return gulp.src('test/*.js')
     .pipe(mocha());
 });
 
@@ -13,12 +13,11 @@ gulp.task('lint', function() {
   return gulp.src(['**/*.js', '!node_modules/**'])
     .pipe(eslint())
     .pipe(eslint.format())
-    .pipe(eslint.failAfterError);
+    .pipe(eslint.failAfterError());
 });
 
-var watcher = gulp.watch('**/*.js', ['lint', 'test']);
-watcher.on('change', function() {
-  console.log('File ' + event.path + ' was ' + event.type + 'running tasks...');
+gulp.task('watch', function() {
+  gulp.watch('**/*.js', ['lint', 'test']);
 });
 
-gulp.task('default', ['test', 'lint']);
+gulp.task('default', ['test', 'lint', 'watch']);

349cddf4f47e0b0009dccf82912dceced9abb068
Jacob Knaack <jacobknaack@Jacobs-MacBook-Pro-2.local>
2016-05-07 12:58:55 -0700

    completed testing

--- lab-jacob/test/read-test.js
+++ lab-jacob/test/read-test.js
@@ -4,8 +4,8 @@ const read = require(__dirname + '/../lib/read');
 const main = require(__dirname + '/../index');
 const expect = require('chai').expect;
 
-describe('testing the read', function() {
-  describe('testing if files are read', function() {
+describe('testing the read module', function() {
+  describe('testing if files are read properly', function() {
     it ('should log output "here is text file one"', function(done) {
       read('one.txt', function(err, data) {
         expect(err).to.equal(null);
@@ -14,9 +14,21 @@ describe('testing the read', function() {
       });
     });
 
-    it ('should return an array of [ "68657265206973", "616e6420686572", "616e642066696e" ]', function(done) {
-      expect(main).to.equal('[ \'68657265206973\', \'616e6420686572\', \'616e642066696e\' ]');
-      done();
+    it ('should return an array of [ "6865726520697320", "616e642068657265", "616e642066696e61" ]', function(done) {
+      var results = [];
+      var count = 0;
+      main.files.forEach(function(path, index) {
+        read(path, function(err, data) {
+          results[index] = data.toString('hex', 0, 8);
+          count++;
+          if (count === main.files.length) {
+            console.log(results);
+            expect(err).to.equal(null);
+            expect(results).to.deep.equal(['6865726520697320', '616e642068657265', '616e642066696e61']);
+            done();
+          }
+        });
+      });
     });
   });
 });

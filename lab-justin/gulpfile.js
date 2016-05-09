02ac2921c3d30bf2eec5c4efe8ed980201bdaa00
%%GITNAME%% <%%GITEMAIL%%>
2016-05-08 19:24:57 -0700

    gulpfile has been updated

--- lab-justin/gulpfile.js
+++ lab-justin/gulpfile.js
@@ -1,18 +1,26 @@
-'use strict';
+'run strict';
 
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
-const eslint = require('eslint');
-// const chai = require('chai');
+const eslint = require('gulp-eslint');
 
-gulp.task('default', function(){
-  return gulp.src('test/*.js')
+var paths = ['**/*.js', 'test/*.js'];
+
+gulp.task('mocha', function(){
+  return gulp.src('test/*.js', !'node_modules')
   .pipe(mocha());
 });
 
 gulp.task('eslint', function(){
-  return gulp.src('*.js')
+  return gulp.src(paths)
   .pipe(eslint())
   .pipe(eslint.format())
   .pipe(eslint.failAfterError());
 });
+
+gulp.task('watch', ()=>{
+  gulp.watch('**/*.js', !'package.json', ['eslint']);
+  gulp.watch('test/*.js', ['eslint', 'mocha']);
+
+});
+gulp.task('default', ['eslint', 'mocha', 'watch']);

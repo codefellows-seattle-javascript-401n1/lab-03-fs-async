14da55c40dd7b0bb4d667b617807c83447a27117
Andrew Watson <andrewpeterwatson@gmail.com>
2016-05-12 16:34:53 -0700

    cleaning code

--- lab-andrew/gulpfile.js
+++ lab-andrew/gulpfile.js
@@ -2,21 +2,15 @@
 
 const gulp = require('gulp');
 const mocha = require('gulp-mocha');
-// const eslint = require('gulp-eslint');
-
-
+const eslint = require('gulp-eslint');
 
 gulp.task('test', function() {
   return gulp.src(__dirname + '/test/*.js')
   .pipe(mocha());
 });
-// gulp.task('lint', function() {
-//   return gulp.src(['**/*.js', '!node_modules/**'])
-//   .pipe(eslint()).pipe(eslint.format());
-// });
-var watcher = gulp.watch(['**/*.js','!node_modules**'], ['test']);
-watcher.on('change', function(event) {
-  console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
+gulp.task('lint', function() {
+  return gulp.src(['**/*.js', '!node_modules/**'])
+  .pipe(eslint()).pipe(eslint.format());
 });
-gulp.task('default', ['test'], function() {
+gulp.task('default', ['test', 'lint'], function() {
 });

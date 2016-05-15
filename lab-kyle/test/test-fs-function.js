c3b5bc5869dcd122ffeabae83b82c06c371aa4fc
kyleschnirring <kyleschnirring@gmail.com>
2016-05-15 12:46:24 -0700

    i think i got everything

--- lab-kyle/test/test-fs-function.js
+++ lab-kyle/test/test-fs-function.js
@@ -5,7 +5,7 @@ const expect = require('chai').expect;
 
 describe('testing module lib/fs-function.js', function() {
   describe('testing function bufferfiles()', function() {
-    it('should return the first 8 bits of a text file in hex ', function(done){
+    it('should return the first 8 bits of a text file in hex ', function(err, done){
       read.bufferfiles(__dirname + '/../data/one.txt', function(err, data){
         expect(err).to.equal(err);
         expect(data.toString('hex', 0, 8)).to.equal('68656c6c6f20636f');

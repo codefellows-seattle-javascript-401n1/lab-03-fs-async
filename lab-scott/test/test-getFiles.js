4ff79b54aa32f7bb02e17b2140f7ebad8bac0d9a
Scott Schmidt <sjschmidt44@gmail.com>
2016-05-17 11:45:44 -0700

    Add blank test

--- lab-scott/test/test-getFiles.js
+++ lab-scott/test/test-getFiles.js
@@ -1,1 +1,9 @@
 'use strict';
+
+describe('Testing module lib/getFiles.js', function() {
+  describe('Testing function readFile()', function() {
+    it('Should pass; no test', function() {
+
+    });
+  });
+});

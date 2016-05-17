e406c47db9ab2ec3bf0bfe2920cf5807c275b070
Jacob Knaack <jacobknaack@internalcheck.apple.com>
2016-05-17 10:06:34 -0700

    added comments for assignment resubmission

--- lab-jacob/index.js
+++ lab-jacob/index.js
@@ -2,12 +2,12 @@
 
 const read = require(__dirname + '/lib/read');
 const files = ['one.txt', 'two.txt', 'three.txt'];
-exports.files = files;
+exports.files = files; // puts file extensions on the exports module for use in tests
 
-var results = [];
-var count = 0;
+var results = []; // array where hex data will be placed after buffereing
+var count = 0;    // count variable, causes results to be loged after all buffer data has been placed in results
 
-files.forEach(function(path, index) {
+files.forEach(function(path, index) { // goes through each file, runs the read modules and places the data into results array
   read(path, function(err, data) {
     if (err) {
       console.error('broken');
--- lab-jacob/lib/read.js
+++ lab-jacob/lib/read.js
@@ -2,6 +2,7 @@
 
 const fs = require('fs');
 
+// module for reading creating buffer data from data folder
 module.exports = function(file, callback) {
   fs.readFile('./data/' + file, function(err, data) {
     if (err) throw err;

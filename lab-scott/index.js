c658497a0e58444f3f086cb51cb74a8bf85ecdce
Scott Schmidt <sjschmidt44@gmail.com>
2016-05-18 14:49:42 -0700

    add lib getFiles and callback to index.js method

--- lab-scott/index.js
+++ lab-scott/index.js
@@ -1,1 +1,15 @@
 'use strict';
+
+const getFiles = require(__dirname + '/lib/getFiles');
+const files = ['one.txt', 'two.txt', 'three.txt'];
+
+const filePaths = files.map(function(file) {
+  return './data/' + file;
+});
+
+getFiles(filePaths, function(err, fd) {
+  if (err) console.error(err);
+  fd.forEach(function(val) {
+    console.log(val);
+  });
+});
--- lab-scott/test/test-getFiles.js
+++ lab-scott/test/test-getFiles.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const assert = require('assert');
+const getFiles = require(__dirname + '../lib/getFiles');
+
 describe('Testing module lib/getFiles.js', function() {
   describe('Testing function readFile()', function() {
     it('Should pass; no test', function() {

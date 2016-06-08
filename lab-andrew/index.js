a1ea8d004c176de88767896f69e239ee820e91df
Andrew Watson <andrewpeterwatson@gmail.com>
2016-06-07 17:56:12 -0700

    reafactored and finished  testing

--- lab-andrew/index.js
+++ lab-andrew/index.js
@@ -1,39 +1,8 @@
 'use strict';
 
+const readFiles = require(__dirname + '/lib/readFiles');
+var paths = ['/data/one.txt', '/data/two.txt', '/data/three.txt'];
 
-// const readFile = require(__dirname + '/lib/control');
-
-const first = require(__dirname + '/lib/onecontrol');
-const second = require(__dirname + '/lib/control');
-const third = require(__dirname + '/lib/threecontrol');
-var dataArr = [];
-var counter = 0;
-
-const handleData = module.exports = function(file) {
-  first(file, function(data) {
-    dataArr[0] = data;
-    counter++;
-    controlData();
-  });
-  second(file, function(data) {
-    dataArr[1] = data;
-    counter++;
-    controlData();
-  });
-  third(file, function(data) {
-    dataArr[2] = data;
-    counter++;
-    controlData();
-  });
-
-  function controlData(cb) {
-    if (counter === 3) {
-      console.log(dataArr);
-      return cb ? cb(dataArr) : dataArr;
-    }
-  }
-};
-
-handleData(function(data) {
+readFiles(paths, function(data) {
   console.log(data);
 });
--- lab-andrew/test/asynctest.js
+++ lab-andrew/test/asynctest.js
@@ -1,13 +1,12 @@
 'use strict';
-const handleData = require(__dirname + '/../index');
+const readFiles = require(__dirname + '/../lib/readFiles');
 const expect = require('chai').expect;
-
+var paths = ['/data/one.txt', '/data/two.txt', '/data/three.txt'];
 describe('testing index', function() {
   describe('testing modules', function() {
     it('Should return first 8 hex', function(done) {
-      handleData(function(data) {
-        expect(data).to.be.eql('4920616d206f6e65', '4920616d2074776f',
-'4920616d20746872');
+      readFiles(paths, function(data) {
+        expect(data).to.be.eql([ '4920616d206f6e65', '4920616d2074776f', '4920616d20746872' ]);
         done();
       });
     });
